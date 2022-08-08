57728779f6559e1fdc8485271779d8dbb909bb4d
nastya <myosotis1990@mail.ru>
2022-08-08 14:07:48 +0700

    xftdhstysyeee

--- src/burka/fib.py
+++ src/burka/fib.py
@@ -1,8 +1,8 @@
-n1 = 1
-n2 = 1
-i = 1
+n1 = 0
+n2 = 0
+i = 0
 
-while i < 15:
+while i <= 15:
     n_sum = n1 + n2
     n1 = n2
     n2 = n_sum
---
+++ src/burka/ya_razvlekayus.py
@@ -0,0 +1,14 @@
+n = 3
+y = 1
+while n > 1:
+    n = n - 1
+    y = y + n
+    print(y)
+
+n = 7
+i = 0
+y = 1
+while i<=n:
+    i = i + 1
+    y += i
+    print (y)
